b0605e7313fc0446d6ea1e50aa9b657aeb62d7d1
Deltonjr2 <Demetria.Farewell@yahoo.com>
2019-09-06 08:58:53 -0500

    retake

---
+++ pleaselord!.py
@@ -0,0 +1,52 @@
+# You must push BEFORE 9:00am or your answers will not be graded.
+#
+# Do NOT use any notes or previous projects on this test. Each individual question file has the same questions as the README.
+
+# Ask a user for their first name and age. Print "[NAME] is [AGE] years old"
+# userInput1=  input("Please enter your first name")
+# userInput2=  input("Please enter your age")
+# print(userInput1) + (userInput2)
+
+# Ask the user to enter their age. Check if they entered a value between 0 and 125. If valid, print "REAL AGE". If invalid print “NOT A REAL AGE!!!”
+# userInput= input("Enter your age?")
+# if userinput  > 0  > 125:
+#  print ("Real Age")
+# elif userinput < 0 < 125:
+#  print ("Not a real age")
+# Ask the user to enter a number to add to a total. Keep asking the user to enter a number until they enter 0. Afterward, print the total of all numbers entered.
+# userInput= input("Enter a number to add to a total")
+# while userInput !=0:
+#     print("total of all numbers entered")
+    # Create an array of 4 names. Print one string that has all of the arrays separated by commas.
+# Create a function that’s passed three integer numbers. Print the sum of the first two numbers and return the product of the second two numbers.
+# Create the class Boardgame with name, price, pieceCount, and publisher properties/attributes. Create a class method that will change the price of the book. Outside of the class, create three objects of the class Boardgame. Print the three boardgame objects using the newly created objects.
+class Boardgame:
+    def __init__(self,name,price, pieceCount, Publisther ):
+        self.name=name
+        self.price=price
+        self.pieceCount=PieceCount
+        self.publishter=publisher
+
+# # Create a function that takes a string array and returns a string array with the letter 's' at the end of each element. Call the function.
+# def stringarrayfunction ():
+#     userinput= input("Enter letter that ends in 's'")
+#     return ("string letters that endin s")
+stringarrayfunction()
+#
+# def numberfunction (integerArray):
+#     integerArray= [2,4,6,5,3]
+# print()
+#integerArray ():
+# Create a Puppy class. It should have properties name and color. Create a program that will ask a user to enter the name, then the color of a puppy until they enter 'q' to quit. Put each entry in an array.
+# class Puppy:
+#     def __init__(self, bulldog, black):
+#         self.bulldog=bulldog
+#         self.black=black
+#
+# userInput1=("Enter a puppy name")
+# userInput2=("Enter a puppy color")
+# while userInput1 & userInput2 !=q:
+#
+#    puppyArray=[Fido,Brown]
+#    for eachElement in (puppyArray):
+#    print (puppyArray)
